a187692ad44a0a81a98031088399dc8b86cb5ee2
Alexandre HERRERO <alexandre.herrero@live.fr>
2016-12-07 12:34:45 +0100

    mod

---
+++ stream.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: STREAM
+ */
+
+
+get_header();
+
+?>
+<body>
+<section id="section1">
+    <div class="container-fluid container-stream">
+        <div class="row">
+            <div class="col-md-6">
+                <button class="btn-default center-block">TWITCH</button>
+            </div>
+            <div class="col-md-6">
+                <button class="btn-default center-block">YOUTUBE</button>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
