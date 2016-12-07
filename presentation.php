9d17a922aedc0d1914b86c2c6522e61d5a3a8bd5
Alexandre HERRERO <alexandre.herrero@live.fr>
2016-12-07 11:18:04 +0100

    ajout page prez

---
+++ presentation.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * Template Name: PRESENTATION
+ */
+
+
+get_header();
+
+?>
+    <script>
+        $(function() {
+            $(".con1").addClass("load");
+            $(".con2").addClass("load");
+        });
+
+    </script>
+	
+<body class="bodycab">
+
+<div class="container-fluid header">
+	<div class="col-md-12 header">
+		<img class="img-responsive" src="<?php bloginfo('url'); ?>/wp-content/themes/dazzling/img/background.jpg" alt="">
+	</div>
+</div>
+
+<div class="container con1">
+	<div class="col-lg-8 col-md-12">
+		<h2>A more intense result</h2>
+		<p>Shortly after the world premiere of the AMG GT R, the sports car and performance brand from Affalterbach is presenting two exciting roadster variants of the two-seater sports car. Alongside the AMG GT Roadster, the AMG GT C Roadster enhances the portfolio with a completely new model variant. “With our two Roadster models, we are strategically expanding the AMG GT family by two exciting variants. With the AMG GT C Roadster, we are also introducing a new model variant to which we have transferred main performance-related components from our top sports car, the AMG GT R. The result: a highly dynamic driving experience in a very exclusive ambience, coupled with that pure roadster feeling,” says Tobias Moers, Chairman of Mercedes-AMG GmbH.</p>
+	</div>
+	<div class="col-lg-4 col-md-12">
+		<img class="img-responsive img-ss-header" src="img/cabh1.jpg" alt="">
+	</div>
+</div>
+
+<div class="container con2">
+	<div class="col-lg-4 col-md-12">
+		<img class="img-responsive img-ss-header" src="img/cabh2.jpg" alt="">
+	</div>
+	<div class="col-lg-8 col-md-12">
+		<h2>New stage: Peak Power</h2>
+		<p>With the AMG GT C Roadster, Mercedes-AMG is introducing a further output level of the 4.0-litre V8 biturbo, thereby underlining the exclusive character of the new model. At 410 kW (557 hp), the peak output is 35 kW (47 hp) above that of the AMG GT S and 20 kW (28 hp) below the Mercedes-AMG GT R. The maximum torque of 680 newton metres is available from 1900 to 5750 rpm. A sprint from zero to 100 km/h is absolved in 3.7 seconds, and this fulminating acceleration only ends at the top speed of 316 km/h.</p>
+	</div>
+</div>
+
+
+<div class="container-cab">
+<div class="col-md-6 coll">
+	<div class="hovereffect1">
+		<img class="img-responsive" src="img/cab1.jpg" alt="">
+		<div class="overlay1">
+		</div>
+	</div>
+
+	<div class="hovereffect1">
+		<img class="img-responsive" src="img/cab2.jpg" alt="">
+		<div class="overlay1">
+		</div>
+	</div>
+</div>
+</div>
+
+<div class="container-img">
+	<div class="col-md-6 colr">
+		<div class="hovereffect1">
+			<img class="img-responsive" src="img/cab3.jpg" alt="">
+			<div class="overlay1">
+			</div>
+
+		</div>
+
+		<div class="hovereffect1">
+			<img class="img-responsive" src="img/cab4.jpg" alt="">
+			<div class="overlay1">
+			</div>
+		</div>
+	</div>
+</div>
+
+
+</body>
+<!-- footer -->
+<footer>
+	<center>
+		<footer>
+			<div  class="hidden-xs">
+				<div class="container-footer">
+					<div id="footer" class="row amgrow">
+						<div class="col-sm-3 col-xs-6">
+							<h4>Mercedes</h4>
+							<a href="#">Configurateur</a><br>
+							<a href="#">Comparer modèles</a><br>
+							<a href="#">Rechercher un véhicule</a><br>
+							<a href="#">Concessionnaires</a><br>
+						</div>
+						<div class="col-sm-3 col-xs-6">
+							<h4>Le site</h4>
+							<a href="amg.html">AMG GT-S</a><br>
+							<a href="cab.html">Cabriolet</a><br>
+							<a href="story2.html">Histoire</a><br>
+							<a href="contact2.html">Contact</a><br>
+						</div>
+						<div class="col-sm-3 col-xs-6">
+							<h4>Services</h4>
+							<a href="#">Financement</a><br>
+							<a href="#">Login</a><br>
+							<a href="#">News</a><br>
+							<a href="#">Contact</a><br>
+						</div>
+						<div class="col-sm-3 col-xs-6">
+							<h4>Réseaux sociaux</h4>
+							<div class="social">
+								<a class="social1" href="https://www.facebook.com/MercedesBenzFrance/" target="_blank" title="Facebook" ><img src="img/fb.png"></a>
+								<a class="social2" href="https://twitter.com/mercedesbenz" target="_blank" title="Twitter" ><img src="img/twitter.png"></a>
+								<a class="social3" href="https://www.youtube.com/user/MercedesBenzTV" target="_blank" title="YouTube" ><img src="img/yt.png"></a>
+								<a class="social4" href="https://www.instagram.com/mercedesbenz/" target="_blank" title="Instagram" ><img src="img/insta.png"></a>
+							</div>
+							<h4>Communauté</h4>
+							<a href="http://www.forum-mercedes.com/" target="_blank">Forum Officiel</a><br>
+						</div>
+						<div class="col-sm-12 "><hr class=sepbas width= 32%  size=4></div>
+						<div id="copyright" class="col-sm-12 "><h5>Copyright 2016 © Alexandre Herrero // Marwan Diridollou</h5></div>
+					</div>
+				</div>
+			</div>
+	</center>
+
+	<div class="visible-xs">
+		<div id="footermobile" class="container">
+			<div  class="col-xs-12">
+                <br>
+				<a href="index.html">Accueil</a><br>
+				<hr>
+				<a href="amg.html">AMG GT-S</a><br>
+				<hr>
+				<a href="cab.html">Cabriolet</a><br>
+				<hr>
+				<a href="story2.html">Histoire</a><br>
+				<hr>
+				<a href="contact2.html">Contact</a><br>
+				<hr>
+				<div>
+					<a class="social1" href="https://www.facebook.com/MercedesBenzFrance/" target="_blank" title="Facebook" ><img src="img/fb.png"></a>
+					<a class="social2" href="https://twitter.com/mercedesbenz" target="_blank" title="Twitter" ><img src="img/twitter.png"></a>
+					<a class="social3" href="https://www.youtube.com/user/MercedesBenzTV" target="_blank" title="YouTube" ><img src="img/yt.png"></a>
+					<a class="social4" href="https://www.instagram.com/mercedesbenz/" target="_blank" title="Instagram" ><img src="img/insta.png"></a>
+				</div>
+				<hr>
+				<center><div id="copyright" class="col-sm-12 "><h5>Copyright 2016 © Alexandre Herrero // Marwan Diridollou</h5></div></center>
+			</div>
+		</div>
+	</div>
+</footer>
